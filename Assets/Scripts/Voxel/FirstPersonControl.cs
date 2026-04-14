fd4f773da4024e5120c018c2f55f04003991b4ae
NikiMaisu <nikoloz.maisuradze.06@gmail.com>
2026-04-14 20:15:00 +0200

    add first person movement and input handling

---
+++ Assets/Scripts/Voxel/FirstPersonControl.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class FirstPersonControl : MonoBehaviour
+{
+    
+    public float forwardSpeed = 11f;
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Keyboard.current.wKey.isPressed) {
+            transform.transform.Translate(new Vector3(0, 0, (forwardSpeed * Time.deltaTime)));
+        }
+        
+        if (Keyboard.current.sKey.isPressed) {
+            transform.transform.Translate(new Vector3(0, 0, (forwardSpeed * Time.deltaTime) * -1f));
+        }
+        
+        if (Keyboard.current.aKey.isPressed) {
+            transform.transform.Translate(new Vector3((forwardSpeed * Time.deltaTime) * -1f, 0, 0));
+        }
+        
+        if (Keyboard.current.dKey.isPressed) {
+            transform.transform.Translate(new Vector3((forwardSpeed * Time.deltaTime), 0, 0));
+        }
+        if (Keyboard.current.spaceKey.isPressed) {
+            transform.transform.Translate(new Vector3(0, (forwardSpeed * Time.deltaTime), 0));
+        }
+        if (Keyboard.current.shiftKey.isPressed) {
+            transform.transform.Translate(new Vector3(0, (forwardSpeed * Time.deltaTime) * -1, 0));
+        }
+    }
+}
