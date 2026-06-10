8775c22c66c5e0d15c4b3b387f68f5d9b60888fe
NikiMaisu <nikoloz.maisuradze.06@gmail.com>
2026-06-10 17:20:00 +0200

    support placing different block types instead of one hardcoded block

--- Assets/Scripts/Voxel/Chunk.cs
+++ Assets/Scripts/Voxel/Chunk.cs
@@ -22,6 +22,8 @@ public class Chunk : MonoBehaviour
     {
         blocks = new BlockType[Width, Height, Depth];
         blocks[0, 0, 0] = BlockType.Bedrock;
+        blocks[2, 0, 3] = BlockType.Bedrock;
+        blocks[5, 0, 1] = BlockType.Bedrock;
 
         MeshFilter meshFilter = GetComponent<MeshFilter>();
         MeshRenderer meshRenderer = GetComponent<MeshRenderer>();
@@ -42,7 +44,7 @@ public class Chunk : MonoBehaviour
         List<Vector3> vertices = new List<Vector3>();
         List<int> triangles = new List<int>();
         List<Vector2> uvs = new List<Vector2>();
-
+        
         void AddFace(Vector3 v0, Vector3 v1, Vector3 v2, Vector3 v3)
         {
             int startIndex = vertices.Count;
@@ -64,13 +66,23 @@ public class Chunk : MonoBehaviour
             triangles.Add(startIndex + 2);
             triangles.Add(startIndex + 3);
         }
-        
-        AddFace(new Vector3(0, 0, 1), new Vector3(1, 0, 1), new Vector3(1, 1, 1), new Vector3(0, 1, 1));
-        AddFace(new Vector3(1, 0, 0), new Vector3(0, 0, 0), new Vector3(0, 1, 0), new Vector3(1, 1, 0));
-        AddFace(new Vector3(0, 0, 0), new Vector3(0, 0, 1), new Vector3(0, 1, 1), new Vector3(0, 1, 0));
-        AddFace(new Vector3(1, 0, 1), new Vector3(1, 0, 0), new Vector3(1, 1, 0), new Vector3(1, 1, 1));
-        AddFace(new Vector3(0, 0, 0), new Vector3(1, 0, 0), new Vector3(1, 0, 1), new Vector3(0, 0, 1));
-        AddFace(new Vector3(0, 1, 1), new Vector3(1, 1, 1), new Vector3(1, 1, 0), new Vector3(0, 1, 0));
+
+        for (int x = 0; x < Width; x++) {
+            for (int y = 0; y < Height; y++) {
+                for (int z = 0; z < Depth; z++)
+                {
+                    if (blocks[x, y, z] == BlockType.Bedrock)
+                    {
+                        AddFace(new Vector3(0 + x, 0 + y, 1 + z), new Vector3(1 + x, 0 + y, 1 + z), new Vector3(1 + x, 1 + y, 1 + z), new Vector3(0 + x, 1 + y, 1 + z));
+                        AddFace(new Vector3(1 + x, 0 + y, 0 + z), new Vector3(0 + x, 0 + y, 0 + z), new Vector3(0 + x, 1 + y, 0 + z), new Vector3(1 + x, 1 + y, 0 + z));
+                        AddFace(new Vector3(0 + x, 0 + y, 0 + z), new Vector3(0 + x, 0 + y, 1 + z), new Vector3(0 + x, 1 + y, 1 + z), new Vector3(0 + x, 1 + y, 0 + z));
+                        AddFace(new Vector3(1 + x, 0 + y, 1 + z), new Vector3(1 + x, 0 + y, 0 + z), new Vector3(1 + x, 1 + y, 0 + z), new Vector3(1 + x, 1 + y, 1 + z));
+                        AddFace(new Vector3(0 + x, 0 + y, 0 + z), new Vector3(1 + x, 0 + y, 0 + z), new Vector3(1 + x, 0 + y, 1 + z), new Vector3(0 + x, 0 + y, 1 + z));
+                        AddFace(new Vector3(0 + x, 1 + y, 1 + z), new Vector3(1 + x, 1 + y, 1 + z), new Vector3(1 + x, 1 + y, 0 + z), new Vector3(0 + x, 1 + y, 0 + z));
+                    }
+                }
+            }
+        }
         
         mesh.vertices = vertices.ToArray();
         mesh.triangles = triangles.ToArray();
